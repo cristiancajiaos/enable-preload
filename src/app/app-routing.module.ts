2e2f6c98c896ba14b88705a3813d6eb390a25107
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-05-04 12:53:44 -0400

    Definida estrategia de carga

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{ path: 'foo', loadChildren: () => import('./foo/foo.module').then(m => m.FooModule) }, { path: 'bar', loadChildren: () => import('./bar/bar.module').then(m => m.BarModule) }, { path: 'baz', loadChildren: () => import('./baz/baz.module').then(m => m.BazModule) }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
